8d9f3c7233dc21cca87d793a0c83baf6ead4e3b6
Aouatif Hamouch <0101aohamouch@student.epfc.eu>
2023-10-31 11:32:45 +0100

    feat(add_templates): finalisation

--- src/main/java/tgpr/tricount/TricountApp.java
+++ src/main/java/tgpr/tricount/TricountApp.java
@@ -2,7 +2,12 @@ package tgpr.tricount;
 
 import tgpr.framework.Controller;
 import tgpr.framework.Model;
+import tgpr.tricount.controller.AddTemplateController;
 import tgpr.tricount.controller.TestController;
+import tgpr.tricount.model.Repartition;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TricountApp {
     public final static String DATABASE_SCRIPT_FILE = "/database/tgpr-2324-a04.sql";
@@ -10,8 +15,12 @@ public class TricountApp {
     public static void main(String[] args) {
         if (!Model.checkDb(DATABASE_SCRIPT_FILE))
             Controller.abort("Database is not available!");
+
         else {
-            Controller.navigateTo(new TestController());
+            List<Repartition> repartitions = new ArrayList<>();
+            repartitions.add(new  Repartition(2, 1, 1));
+            repartitions.add(new Repartition(2, 2, 1));
+            Controller.navigateTo(new AddTemplateController(repartitions, 4 ));
         }
     }
 }
--- src/main/java/tgpr/tricount/controller/AddTemplateController.java
+++ src/main/java/tgpr/tricount/controller/AddTemplateController.java
@@ -1,2 +1,47 @@
-package tgpr.tricount.controller;public class AddTemplateController {
+package tgpr.tricount.controller;
+
+import com.googlecode.lanterna.gui2.Window;
+import tgpr.framework.Controller;
+import tgpr.framework.Error;
+import tgpr.framework.ErrorList;
+import tgpr.tricount.model.Repartition;
+import tgpr.tricount.model.Template;
+import tgpr.tricount.model.TemplateItem;
+import tgpr.tricount.model.Tricount;
+import tgpr.tricount.view.AddTemplateView;
+
+import java.util.List;
+
+public class AddTemplateController extends Controller {
+    private List<Repartition> repartitions;
+    int tricountId;
+    public AddTemplateController(List<Repartition> repartitions, int tricountId) {
+        this.repartitions = repartitions;
+        this.tricountId = tricountId;
+    }
+    public static Error isValidTitle(String title) {
+        if (title != null && !title.trim().isEmpty())
+            return null;
+        return new Error("Title is required", Template.Fields.Title);
+    }
+    public ErrorList validate(String title) {
+        var errors = new ErrorList();
+        errors.add(isValidTitle(title));
+        return errors;
+    }
+    public void create(String title){
+        Template template = new Template(title, tricountId);
+        template.save();
+        for (var rep: repartitions) {
+            TemplateItem templateItem = new TemplateItem();
+            templateItem.setUserId(rep.getUserId());
+            templateItem.setWeight(rep.getWeight());
+            templateItem.setTemplateId(template.getId());
+            templateItem.save();
+        }
+    }
+    @Override
+    public Window getView() {
+        return new AddTemplateView(this);
+    }
 }
