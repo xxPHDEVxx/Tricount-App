21535c20e75499af16e1eb559118636a84b02622
MartinWester <martin.driss.wester@gmail.com>
2023-11-08 00:19:49 +0100

    feat(login): Début implémentation LoginView et LoginController

--- src/main/java/tgpr/tricount/controller/LoginController.java
+++ src/main/java/tgpr/tricount/controller/LoginController.java
@@ -1,4 +1,47 @@
 package tgpr.tricount.controller;
 
-public class LoginController {
+import com.googlecode.lanterna.gui2.Window;
+import tgpr.tricount.TricountApp;
+import tgpr.tricount.model.User;
+import tgpr.tricount.model.MemberValidator;
+import tgpr.tricount.model.Security;
+import tgpr.tricount.view.LoginView;
+import tgpr.framework.Controller;
+import tgpr.framework.Error;
+import tgpr.framework.ErrorList;
+import tgpr.framework.Model;
+
+import java.util.List;
+
+public class LoginController extends Controller {
+    public void exit() {
+        System.exit(0);
+    }
+
+    public List<Error> login(String pseudo, String password) {
+        var errors = new ErrorList();
+        errors.add(MemberValidator.isValidPseudo(pseudo));
+        errors.add(MemberValidator.isValidPassword(password));
+
+        if (errors.isEmpty()) {
+            var member = User.checkCredentials(pseudo, password);
+            if (member != null) {
+                Security.login(member);
+                navigateTo(new MemberListController());
+            } else
+                showError(new Error("invalid credentials"));
+        } else
+            showErrors(errors);
+
+        return errors;
+    }
+
+    public void seedData() {
+        Model.seedData(TricountApp.DATABASE_SCRIPT_FILE);
+    }
+
+    @Override
+    public Window getView() {
+        return new LoginView(this);
+    }
 }
--- src/main/java/tgpr/tricount/view/LoginView.java
+++ src/main/java/tgpr/tricount/view/LoginView.java
@@ -2,20 +2,19 @@ package tgpr.tricount.view;
 
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.input.KeyStroke;
-import tgpr.framework.Configuration;
-import tgpr.framework.Margin;
-import tgpr.framework.Spacing;
 import tgpr.tricount.controller.LoginController;
+import tgpr.framework.Configuration;
+import tgpr.framework.Layouts;
+import tgpr.framework.ViewManager;
 
 import java.util.List;
 
 public class LoginView extends BasicWindow {
 
     private final LoginController controller;
-
-    private final TextBox txtPseudo = new TextBox();
-    private final TextBox txtPassword = new TextBox();
-    private Button btnLogin;
+    private final TextBox txtPseudo;
+    private final TextBox txtPassword;
+    private final Button btnLogin;
 
     public LoginView(LoginController controller) {
         this.controller = controller;
@@ -23,58 +22,39 @@ public class LoginView extends BasicWindow {
         setTitle("Login");
         setHints(List.of(Hint.CENTERED));
 
-        Panel root = Panel.verticalPanel(1);
+        Panel root = new Panel();
         setComponent(root);
 
-        createFieldsPanel().addTo(root);
-        createButtonsPanel().addTo(root);
-        createDebugPanel().addTo(root);
+        Panel panel = new Panel().setLayoutManager(new GridLayout(2).setTopMarginSize(1).setVerticalSpacing(1))
+                .setLayoutData(Layouts.LINEAR_CENTER).addTo(root);
+        panel.addComponent(new Label("User:"));
+        txtPseudo = new TextBox().addTo(panel);
+        panel.addComponent(new Label("Password:"));
+        txtPassword = new TextBox().setMask('*').addTo(panel);
 
-        btnLogin.takeFocus();
-    }
+        new EmptySpace().addTo(root);
 
-    private Panel createFieldsPanel() {
-        Panel panel = Panel.gridPanel(2,
-                        Margin.of(1, 1, 1, 0),
-                        Spacing.of(1))
-                .center();
+        Panel buttons = new Panel().setLayoutManager(new LinearLayout(Direction.HORIZONTAL))
+                .setLayoutData(Layouts.LINEAR_CENTER).addTo(root);
+        btnLogin = new Button("Login", this::login).addTo(buttons);
+        Button btnExit = new Button("Exit", this::exit).addTo(buttons);
 
-        new Label("User:").addTo(panel);
-        txtPseudo.addTo(panel).takeFocus();
+        new EmptySpace().addTo(root);
 
-        new Label("Password:").addTo(panel);
-        txtPassword.setMask('*').addTo(panel);
-
-        return panel;
-    }
-
-    private Panel createButtonsPanel() {
-        Panel panel = Panel.horizontalPanel(1).center();
-        btnLogin = new Button("Login", this::login).addTo(panel);
-        Button btnExit = new Button("Exit", this::exit).addTo(panel);
-
-        addShortcut(btnLogin, KeyStroke.fromString("<A-l>"));
-        addShortcut(btnExit, KeyStroke.fromString("<A-e>"));
-
-        return panel;
-    }
-
-    private Border createDebugPanel() {
         Button btnSeedData = new Button("Reset Database", this::seedData);
-        Panel panel = Panel.verticalPanel(LinearLayout.Alignment.Center,
+        Panel debug = Panel.verticalPanel(LinearLayout.Alignment.Center,
                 new Button("Login as default admin", this::logAsDefaultAdmin),
                 new Button("Login as default member", this::logAsDefaultMember),
                 btnSeedData
         );
-        Border border = panel.withBorder(Borders.singleLine(" For debug purpose "));
+        debug.withBorder(Borders.singleLine(" For debug purpose ")).addTo(root);
 
-        addShortcut(btnSeedData, KeyStroke.fromString("<A-r>"));
-
-        return border;
+        txtPseudo.takeFocus();
     }
 
     private void seedData() {
         controller.seedData();
+        btnLogin.takeFocus();
     }
 
     private void exit() {
