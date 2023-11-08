3a649f1d165b52839d01dc8158966cfee4438b09
Nicolas Vanhorle <vanhorlenicolas@gmail.com>
2023-11-09 00:01:18 +0100

    feat(view_tricount): suppression des conflits pour le merge V2

--- src/main/java/tgpr/tricount/TricountApp.java
+++ src/main/java/tgpr/tricount/TricountApp.java
@@ -3,9 +3,6 @@ package tgpr.tricount;
 import tgpr.framework.Controller;
 import tgpr.framework.Model;
 import tgpr.tricount.controller.TestController;
-import tgpr.tricount.controller.ViewTricountController;
-import tgpr.tricount.model.Security;
-import tgpr.tricount.model.User;
 
 public class TricountApp {
     public final static String DATABASE_SCRIPT_FILE = "/database/tgpr-2324-a04.sql";
