9beaf15d00cd790011899114516af38b160842b7
Nicolas Vanhorle <vanhorlenicolas@gmail.com>
2023-11-09 10:05:15 +0100

    fix(add_tricount): ajout de la validation des champs

--- src/main/java/tgpr/tricount/controller/AddTricountController.java
+++ src/main/java/tgpr/tricount/controller/AddTricountController.java
@@ -2,6 +2,11 @@ package tgpr.tricount.controller;
 
 import com.googlecode.lanterna.gui2.Window;
 import tgpr.framework.Controller;
+import tgpr.framework.ErrorList;
+import tgpr.tricount.model.Subscription;
+import tgpr.tricount.model.Tricount;
+import tgpr.tricount.model.TricountValidator;
+import tgpr.tricount.model.User;
 import tgpr.tricount.view.AddTricountView;
 
 public class AddTricountController extends Controller {
@@ -10,5 +15,24 @@ public class AddTricountController extends Controller {
     @Override
     public Window getView() {
         return view;
+    }
+
+    public void createTricount(Tricount tricount) {
+        var errors = validate(tricount.getTitle(), tricount.getDescription());
+        if (errors.isEmpty()) {
+            tricount.save();
+            view.close();
+            Controller.navigateTo(new EditTricountController(tricount));
+        } else
+            showErrors(errors);
+    }
+
+    public ErrorList validate(String title, String description) {
+        var errors = new ErrorList();
+
+        errors.add(TricountValidator.isValidTitle(title));
+        errors.add(TricountValidator.isValidDescription(description));
+
+        return errors;
     }
 }
