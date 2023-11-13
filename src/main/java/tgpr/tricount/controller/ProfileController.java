295849c83b350278e794841d7d3aca432ce5ccc4
2905nivanhorle <vanhorlenicolas@gmail.com>
2023-11-13 09:16:33 +0100

    feat(profile): Implementation de la usecase

---
+++ src/main/java/tgpr/tricount/controller/ProfileController.java
@@ -0,0 +1,31 @@
+package tgpr.tricount.controller;
+
+import com.googlecode.lanterna.gui2.Window;
+import tgpr.framework.Controller;
+import tgpr.tricount.model.User;
+import tgpr.tricount.view.ProfileView;
+
+public class ProfileController extends Controller {
+    private final ProfileView view;
+    private final User user;
+    public ProfileController(User user) {
+        this.user = user;
+        view = new ProfileView(this);
+    }
+    @Override
+    public Window getView() {
+        return view;
+    }
+    public String getFullName() {
+        return user.getFullName();
+    }
+    public String getMail() {
+        return user.getMail();
+    }
+    public void editProfile() {
+        //EditProfileController(user);
+    }
+    public void changePasswrd() {
+        //ChangePasswordController(user);
+    }
+}
